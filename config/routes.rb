d5ebdfb92596757c25159a443c701d2ce63c86ba
AngomRanjan <angomranjan@gmail.com>
2022-11-29 11:53:05 +0530

    Manage CORS

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,5 @@ Rails.application.routes.draw do
       resources :greetings, only: [:index]
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  
 end
